6a9db5ec642e7baf99fbdb0248abf741854c5eda
Gravios <justingraboski@gmail.com>
2022-09-14 14:46:35 +0200

    angular tutorial - added hero component

---
+++ angular-tour-of-heroes/src/app/mock-heros.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr. Nice' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magenta' },
+    { id: 16, name: 'RubberDucky' },
+    { id: 17, name: 'DynamoStynamo' },
+    { id: 18, name: 'Dr. Que' },
+    { id: 19, name: 'YoMaMa' },
+    { id: 20, name: 'Whorenado' }
+];
